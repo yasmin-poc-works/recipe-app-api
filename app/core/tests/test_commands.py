9b5b9e132cf1141c58049816ed0d40d499aa73f2
yas88min <87359234+yas88min@users.noreply.github.com>
2023-01-09 18:33:40 +0530

    Configured docker compose and checks to wait for db

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -26,7 +26,7 @@ class CommandTests(SimpleTestCase):
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database when getting Operational Error."""
         patched_check.side_effect = [Psycopg2Error] * 2 + \
-            [OperationalError]  * 3 + [True]
+            [OperationalError] * 3 + [True]
 
         call_command('wait_for_db')
 
