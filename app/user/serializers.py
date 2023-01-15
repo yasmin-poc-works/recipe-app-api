55ad2947ec6cd8e03375f840a5af7d198f7644d1
yas88min <87359234+yas88min@users.noreply.github.com>
2023-01-16 01:15:23 +0530

    Manage the authenticated user.

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -11,7 +11,7 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
-    """Serializers for the user object."""
+    """Serializer for the user object."""
 
     class Meta:
         model = get_user_model()
@@ -21,6 +21,17 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         """Create and return a user with encrypted password."""
         return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        """Update and return user."""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
 
 
 class AuthTokenSerializer(serializers.Serializer):
