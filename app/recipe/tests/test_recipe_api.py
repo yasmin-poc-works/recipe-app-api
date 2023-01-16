5f3ccb481a7205e0fb7731a057ff8a9222275ab0
yas88min <87359234+yas88min@users.noreply.github.com>
2023-01-16 23:17:33 +0530

    Recipe API ViewSet.

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -8,7 +8,7 @@ from django.test import TestCase
 from django.urls import reverse
 
 from rest_framework import status
-from rest_framework import APIClient
+from rest_framework.test import APIClient
 
 from core.models import Recipe
 
