8d07036deded128f3ddf8f7789bf69a3ffe2d0e3
yas88min <87359234+yas88min@users.noreply.github.com>
2023-01-19 00:26:42 +0530

    Tests for the tags API.

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -20,3 +20,7 @@ class RecipeDetailSerializer(RecipeSerializer):
 
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + ['description']
+
+
+class TagSerializer(serializers.Serializer):
+    pass
